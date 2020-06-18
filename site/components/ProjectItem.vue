7c5073b04e6a13cc9a18e01aba87fba194854ec8
MatheusVilas <mats.vilas@gmail.com>
2020-06-17 22:21:48 -0300

    progresso projetos

---
+++ site/components/ProjectItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="project-item">
+    <img class="project-item__bg" :src="projectImage" :alt="projectTitle" />
+    <div class="project-item__info">
+      <p class="project-item__title">{{projectTitle}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class ProjectItem extends Vue {
+  @Prop() projectSlug?: string;
+  @Prop() projectImage?: string;
+  @Prop() projectTitle?: string;
+}
+</script>
+
+<style lang="scss" scoped>
+.project-item {
+  width: 338px;
+  height: 214px;
+  position: relative;
+  overflow: hidden;
+
+  &__bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+
+  &__info {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 50px;
+    width: 100%;
+    background: linear-gradient(
+      180deg,
+      transparent 62%,
+      rgba(0, 0, 0, 0.00345888) 63.94%,
+      rgba(0, 0, 0, 0.014204) 65.89%,
+      rgba(0, 0, 0, 0.0326639) 67.83%,
+      rgba(0, 0, 0, 0.0589645) 69.78%,
+      rgba(0, 0, 0, 0.0927099) 71.72%,
+      rgba(0, 0, 0, 0.132754) 73.67%,
+      rgba(0, 0, 0, 0.177076) 75.61%,
+      rgba(0, 0, 0, 0.222924) 77.56%,
+      rgba(0, 0, 0, 0.267246) 79.5%,
+      rgba(0, 0, 0, 0.30729) 81.44%,
+      rgba(0, 0, 0, 0.341035) 83.39%,
+      rgba(0, 0, 0, 0.367336) 85.33%,
+      rgba(0, 0, 0, 0.385796) 87.28%,
+      rgba(0, 0, 0, 0.396541) 89.22%,
+      rgba(0, 0, 0, 0.4) 91.17%
+    );
+  }
+}
+</style>
--- site/pages/projetos/index.vue
+++ site/pages/projetos/index.vue
@@ -1,13 +1,83 @@
 <template>
-  <h1>projetos</h1>
+  <section class="projects">
+    <div class="container">
+      <h2 class="project__title">Projetos mais recentes</h2>
+      <ul class="project__group" v-if="projects">
+        <li v-for="project in projects" :key="project.id">
+          <project-item
+            :projectSlug="project.slug"
+            :projectImage="project.image"
+            :projectTitle="project.title"
+          />
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { Vue, Component } from "vue-property-decorator";
+import ProjectItem from "../../components/ProjectItem.vue";
 
-export default Vue.extend({
-  components: {}
-});
+interface IProjectItem {
+  id: string;
+  slug: string;
+  image: string;
+  title: string;
+}
+
+@Component({
+  components: {
+    ProjectItem
+  }
+})
+export default class Project extends Vue {
+  projects: IProjectItem[] = [
+    {
+      id: "01",
+      slug: "slug-01",
+      image: require("../../assets/images/project-image.jpg"),
+      title: "slug 01"
+    },
+    {
+      id: "02",
+      slug: "slug-02",
+      image: require("../../assets/images/project-image.jpg"),
+      title: "slug 02"
+    },
+    {
+      id: "03",
+      slug: "slug-03",
+      image: require("../../assets/images/project-image.jpg"),
+      title: "slug 03"
+    },
+    {
+      id: "04",
+      slug: "slug-04",
+      image: require("../../assets/images/project-image.jpg"),
+      title: "slug 04"
+    },
+    {
+      id: "05",
+      slug: "slug-05",
+      image: require("../../assets/images/project-image.jpg"),
+      title: "slug 05"
+    }
+  ];
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.projects {
+  &__title {
+    font-size: 18px;
+    text-align: left;
+    color: #f7f4f4;
+  }
+
+  &__group {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
