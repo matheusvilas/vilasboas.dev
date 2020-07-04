b168d4359a726a468e9a1b0b07faba3574ffab60
MatheusVilas <mats.vilas@gmail.com>
2020-07-04 17:11:26 -0300

    Progresso Modal

--- site/components/Modal.vue
+++ site/components/Modal.vue
@@ -8,7 +8,13 @@
       <div class="modal-container">
         <div class="modal__header">
           <h2 class="modal__title">Site Institucional Schin</h2>
-          <picture></picture>
+          <picture class="modal__banner">
+            <img
+              :src="require('../assets/images/banner-example.png')"
+              alt="Flowers"
+              style="width:auto;"
+            />
+          </picture>
         </div>
         <div class="modal__body">
           <q>
@@ -22,10 +28,10 @@
           </div>
         </div>
         <div class="modal__footer">
-          <button>
+          <a href="google.com">
             Visitar
-            <img src alt />
-          </button>
+            <img src="../assets/images/chevron-right.svg" alt />
+          </a>
         </div>
       </div>
     </div>
@@ -57,12 +63,70 @@ export default class Modal extends Vue {}
 
   &-container {
     padding: 34px 38px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__title {
+    font-size: 22px;
+    font-weight: bold;
+    margin-bottom: 25px;
   }
 
   &__background {
     background: rgba(#000, 0.88);
     width: 100%;
     height: 100%;
+  }
+
+  &__banner {
+    max-height: 270px;
+    width: 100%;
+    display: block;
+  }
+
+  &__body {
+    display: flex;
+    justify-content: space-between;
+
+    q {
+      font-size: 18px;
+      font-weight: 400;
+      max-width: 480px;
+    }
+  }
+
+  &__stack {
+    min-width: 118px;
+    p {
+      font-size: 18px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+  }
+
+  &__header {
+    margin-bottom: 30px;
+  }
+
+  &__footer {
+    margin-top: auto;
+    width: 118px;
+    margin-left: auto;
+
+    a {
+      font-size: 18px;
+      font-weight: bold;
+      color: black;
+      display: flex;
+      justify-content: center;
+      align-content: center;
+
+      img {
+        margin-left: 8px;
+      }
+    }
   }
 
   &__content {
